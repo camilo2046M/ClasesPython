8f89aee3023ebbfa89b301946752a58da3eb8a5f
camilo2046M <ca.menav@duocuc.cl>
2024-06-19 09:38:23 -0400

    Add files via upload

---
+++ Ejercicio 6 Décimas.py
@@ -0,0 +1,31 @@
+cargo=["CEO","Analista de datos","Desarrollador"]
+sueldoBruto=[]
+nombreTrabajador=[]
+while True:
+    print("1.-Registrar Trabajador ")
+    print("2.-Lista de trabajadores ")
+    print("3.-Imprimir planilla de sueldos ")
+    print("4.-Salir del programa" )
+    opcion=int(input("Escoja una opción: "))
+    match opcion:
+        case 1: 
+          print("Complete los siguientes campos ")
+          def nombreTrabajador():
+            nombre=input("Ingrese el nombre del trajador ")
+            apellido=input("Ingrese los apellidos del trabajador ")
+            cargo=input(f"Ingrese un cargo{cargo}")
+            
+
+
+        case 2:
+          print(nombreTrabajador)
+
+
+        case 3:
+          print
+
+
+
+        case 4:
+          break
+    
